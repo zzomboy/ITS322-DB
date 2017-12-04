efa05b80376c28df505f7f0bd029bcad0bd36e72
karanpoj <karanpoj@gmail.com>
2017-12-04 20:31:43 +0700

    fix bug from article_read to activity_read

--- activity.php
+++ activity.php
@@ -13,8 +13,8 @@
 		$layout_footer = new Template("layout_footer_admin.tpl");
 		$adminname = $_SESSION['adminname'];
 	}
-	$layout_header->set('menu_article','class="active"');
-	$layout_header->set('title','Article : Global warming website');
+	$layout_header->set('menu_activity','class="active"');
+	$layout_header->set('title','Activity : Global warming website');
 	echo $layout_header->output();
 ?>
 <!--Content-->
@@ -26,18 +26,18 @@
 			<div class="clear"></div>
 			<h5>Lastest activity</h5>
 			<div class="post_box">
-				<h6><a href="article_read.php"><img src="img/noimgfound.jpg"></a></h6>
-				<h4><a href="article_read.php">ewryt6rujhgvbgkfyui</a></h4>
+				<h6><a href="activity_read.php" target='_blank'><img src="img/noimgfound.jpg"></a></h6>
+				<h4><a href="activity_read.php" target='_blank'>ewryt6rujhgvbgkfyui</a></h4>
 				<p>aoiewhtoigfdocvhjioehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt</p>
 			</div>
 			<div class="post_box">
-				<h6><a href="article_read.php"><img src="img/noimgfound.jpg"></a></h6>
-				<h4><a href="article_read.php">ewryt6rujhgvbgkfyui</a></h4>
+				<h6><a href="activity_read.php" target='_blank'><img src="img/noimgfound.jpg"></a></h6>
+				<h4><a href="activity_read.php" target='_blank'>ewryt6rujhgvbgkfyui</a></h4>
 				<p>aoiewhtoigfdocvhjioehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt</p>
 			</div>
 			<div class="post_box">
-				<h6><a href="article_read.php"><img src="img/noimgfound.jpg"></a></h6>
-				<h4><a href="article_read.php">ewryt6rujhgvbgkfyui</a></h4>
+				<h6><a href="activity_read.php" target='_blank'><img src="img/noimgfound.jpg"></a></h6>
+				<h4><a href="activity_read.php" target='_blank'>ewryt6rujhgvbgkfyui</a></h4>
 				<p>aoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wra</p>
 			</div>
 		</div>
@@ -46,29 +46,29 @@
 			<h3>All activity</h3>
 
 			<div class="list_article">
-				<a href="article_read.php"><img src="img/noimgfound.jpg"></a>
-				<h4><a href="article_read.php">ewryt6rujhgvbgkfyui</a></h4>
+				<a href="activity_read.php" target='_blank'><img src="img/noimgfound.jpg"></a>
+				<h4><a href="activity_read.php" target='_blank'>ewryt6rujhgvbgkfyui</a></h4>
 				<p>aoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wraaoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjaoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:breakktoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:breakktoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;w</p>
 				<h5> 3/12/2017 20:36:52</h5>
 			</div>
 
 			<div class="list_article">
-				<a href="article_read.php"><img src="img/noimgfound.jpg"></a>
-				<h4><a href="article_read.php">ewryt6rujhgvbgkfyui</a></h4>
+				<a href="activity_read.php" target='_blank'><img src="img/noimgfound.jpg"></a>
+				<h4><a href="activity_read.php" target='_blank'>ewryt6rujhgvbgkfyui</a></h4>
 				<p>aoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wraaoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjaoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:breakktoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:breakktoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;w</p>
 				<h5> 3/12/2017 20:36:52</h5>
 			</div>
 
 			<div class="list_article">
-				<a href="article_read.php"><img src="img/noimgfound.jpg"></a>
-				<h4><a href="article_read.php">ewryt6rujhgvbgkfyui</a></h4>
+				<a href="activity_read.php" target='_blank'><img src="img/noimgfound.jpg"></a>
+				<h4><a href="activity_read.php" target='_blank'>ewryt6rujhgvbgkfyui</a></h4>
 				<p>aoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wraaoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjaoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:breakktoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:breakktoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;w</p>
 				<h5> 3/12/2017 20:36:52</h5>
 			</div>
 
 			<div class="list_article">
-				<a href="article_read.php"><img src="img/noimgfound.jpg"></a>
-				<h4><a href="article_read.php">ewryt6rujhgvbgkfyui</a></h4>
+				<a href="activity_read.php" target='_blank'><img src="img/noimgfound.jpg"></a>
+				<h4><a href="activity_read.php" target='_blank'>ewryt6rujhgvbgkfyui</a></h4>
 				<p>aoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wraaoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjaoieektoierhglkjdfil'bjreio[ajtpierjtehtrerektoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:breakktoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:breakktoierhglkjdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;wdfil'bjreio[ajtpierjtpiaerjt word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;word-wrap:break-word;w</p>
 				<h5> 3/12/2017 20:36:52</h5>
 			</div>
