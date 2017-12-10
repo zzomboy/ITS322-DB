1f386831bc12e0e03893c764b55d83e097933b02
karanpoj <karanpoj@gmail.com>
2017-12-10 17:20:35 +0700

    admin checking before use admin page

--- admin.php
+++ admin.php
@@ -9,16 +9,56 @@
 		$layout_footer = new Template("layout_footer.tpl");
 	}
 	else{
-		$layout_header = new Template("layout_header.tpl");
+		$layout_header = new Template("layout_header_admin.tpl");
 		$layout_footer = new Template("layout_footer_admin.tpl");
 		$adminname = $_SESSION['adminname'];
 	}
 	$layout_header->set('title','Admin page : Global warming website');
 	echo $layout_header->output();
+
+/********************** check admin using this page **********************/
+	if(!isset($_SESSION['adminname'])){
+		echo "<p style='margin-top:20px;font-size:26px;text-align:center;'>You don't have permission to use this page.</p>";
+		echo $layout_footer->output();
+		exit();
+	}
+
 ?>
 <!--Content-->
 <!--category & picture-->
-	
+<div class="admin_full">
+	<div class="admin_left">
+		<table class="admin_menu">
+			<tr>
+				<th>
+					Admin page
+				</th>
+			</tr>
+			<tr>
+				<td class="active">
+					<a href="admin.php">Article management</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="admin_activity.php">Activity management</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="admin_addpost.php">New Article & Activity</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="admin_messages.php">View Messages</a>
+				</td>
+			</tr>
+		</table>
+	</div>
+
+	<div class="clear"></div>
+</div>
 <?php
 	echo $layout_footer->output();
 ?>
